ed233b03ca43373fa15bd24361655cd125a4b95a
chaima <chaimamejbri@gmail.com>
2022-03-05 17:00:49 +0100

    fn

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.use(bodyparser.json())
 //
 app.use(express.static(path.join(__dirname,'../','client','build')))
 app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'../','client','/public','index.html'))
+  res.sendFile(path.join(__dirname,'../','client','build','index.html'))
 })
 
 app.get("/", (req, res) => {
