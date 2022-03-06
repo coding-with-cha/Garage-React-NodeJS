4990436d0ea460d5fae9fb5db2ea929f227ed857
chaima <chaimamejbri@gmail.com>
2022-03-06 12:50:40 +0100

    ready

--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -8,7 +8,7 @@ const {CloudinaryStorage} = require('multer-storage-cloudinary')
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
-const cloudConfig = cloudinary.config({
+cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
     api_key:process.env.API_KEY,
     api_secret:process.env.API_SECRET,
