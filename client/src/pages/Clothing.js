237cefd0e1bda98f055e506e403ae00293e19fe7
chaima <chaimamejbri@gmail.com>
2022-03-06 10:47:56 +0100

    ready

--- client/src/pages/Clothing.js
+++ client/src/pages/Clothing.js
@@ -7,7 +7,7 @@ import BtnaddToFav from '../components/BtnaddToFav';
 import Estimation from '../components/Estimation';
 
 const Clothing = () => {
-// 
+
   const dispatch = useDispatch();
   useEffect(()=>{
     dispatch(getPostsFilter("Vêtements"));
@@ -26,11 +26,11 @@ const Clothing = () => {
   {postList && postList.map(post=>(
       <div key={post._id} className="postallpostC" style={{width: "285px"}}>
          <div className="entetePost">
-        <img src='./{post.owner.profilePic}'/>
+        <img src={post.owner.profilePic}/>
         <h4>{post.owner.name}</h4>  
         </div>
         <hr className="hrP"/>
-        <img className="imgPost" src='./{post.postPic}' style={{paddingBottom:"7%", paddingTop:"3%"}}/><br/>
+        <img className="imgPost" src={post.postPic} style={{paddingBottom:"7%", paddingTop:"3%"}}/><br/>
         <h6 className="titlePost">{post.title}</h6><br/>
         <h6 className="descPost">{post.description}</h6><br/>
         <h6 className="prixPost">{post.prix} DT</h6><br/>
