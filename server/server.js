05043751448bedd2749e18b98b294f3926309848
chaima <chaimamejbri@gmail.com>
2022-03-06 02:08:33 +0100

    l com

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use('/api/commande', require('./routes/commandeRoute'))
 
 
 //images path
-app.use('api/uploads', express.static(path.join(__dirname,'../','img-uploads')))
+app.use('/uploads', express.static(path.join(__dirname,'../','img-uploads')))
 
 
 
