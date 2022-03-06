6e37f05f6170ea1bd46e6434b0fe7361b159911b
chaima <chaimamejbri@gmail.com>
2022-03-06 13:15:10 +0100

    ready

--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -7,7 +7,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'img-uploads/')
+      cb(null, './img-uploads')
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + file.originalname
